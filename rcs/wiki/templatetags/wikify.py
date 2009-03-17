d9b47e8ab8ddc024704f01cc307aa3539ec4e22a
arne@rcs4u.de <arne@rcs4u.de@d91cc5cc-1343-0410-b1ed-e1360c4e4ba3>
2009-03-17 07:22:33 +0000

    mark links to inexistant wikipages with a css class.

--- rcs/wiki/templatetags/wikify.py
+++ rcs/wiki/templatetags/wikify.py
@@ -2,6 +2,7 @@
 # copyright Paul Bissex, MIT license
 from django.template import Library
 from django.conf import settings
+from rcs.wiki.models import WikiPage
 
 register = Library()
 
@@ -10,5 +11,13 @@ def wikify(value):
     """Makes WikiWords"""
     import re
     #wikifier = re.compile(r'\b(([A-Z]+[a-z]+){2,})\b')
+    def replace_wikiword(m):
+        slug = m.group(1)
+        try:
+            WikiPage.objects.get(slug=slug)
+            return r' <a href="/wiki/%s/">%s</a>' % (slug, slug)
+        except WikiPage.DoesNotExist:
+            return r' <a class="doesnotexist" href="/wiki/%s/">%s</a>' % (slug, slug)
+        
     wikifier = re.compile(r'[^;/+-]\b(([A-Z]+[a-z]+){2,})\b')
-    return wikifier.sub(r' <a href="/wiki/\1/">\1</a>', value)
+    return wikifier.sub(replace_wikiword, value)
